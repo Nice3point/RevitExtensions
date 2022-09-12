f4ca83c0a030c5cdf9e0bf6f4c3fea76f4896860
Nice3point <nice3point@gmail.com>
2022-09-12 15:51:37 +0300

    Update ParameterExtensions.cs

--- Nice3point.Revit.Extensions/ParameterExtensions.cs
+++ Nice3point.Revit.Extensions/ParameterExtensions.cs
@@ -7,9 +7,7 @@ namespace Nice3point.Revit.Extensions;
 /// </summary>
 public static class ParameterExtensions
 {
-    /// <summary>
-    ///     Provides access to the boolean value within the parameter
-    /// </summary>
+    /// <summary>Provides access to the boolean value within the parameter</summary>
     /// <param name="parameter">The parameter</param>
     /// <returns>The bool value contained in the parameter</returns>
     /// <remarks>The AsBool method should only be used if the StorageType property returns that the internal contents of the parameter is an integer</remarks>
@@ -19,9 +17,7 @@ public static class ParameterExtensions
         return parameter.AsInteger() == 1;
     }
 
-    /// <summary>
-    ///     Provides access to the Color within the parameter
-    /// </summary>
+    /// <summary>Provides access to the Color within the parameter</summary>
     /// <param name="parameter">The parameter</param>
     /// <returns>The Color value contained in the parameter</returns>
     /// <remarks>The AsColor method should only be used if the StorageType property returns that the internal contents of the parameter is an integer</remarks>
@@ -32,9 +28,7 @@ public static class ParameterExtensions
         return new Color((byte) (argb & byte.MaxValue), (byte) ((argb >> 8) & byte.MaxValue), (byte) ((argb >> 16) & byte.MaxValue));
     }
 
-    /// <summary>
-    ///     Provides access to the Element within the parameter
-    /// </summary>
+    /// <summary>Provides access to the Element within the parameter</summary>
     /// <param name="parameter">The parameter</param>
     /// <returns>The element contained in the parameter as an ElementId</returns>
     /// <remarks>The AsElement method should only be used if the StorageType property returns that the internal contents of the parameter is an ElementId</remarks>
@@ -46,9 +40,7 @@ public static class ParameterExtensions
         return elementId == ElementId.InvalidElementId ? null : parameter.Element.Document.GetElement(elementId);
     }
 
-    /// <summary>
-    ///     Provides access to the Element within the parameter
-    /// </summary>
+    /// <summary>Provides access to the Element within the parameter</summary>
     /// <param name="parameter">The parameter</param>
     /// <returns>The element contained in the parameter as an ElementId</returns>
     /// <remarks>The AsElement method should only be used if the StorageType property returns that the internal contents of the parameter is an ElementId</remarks>
