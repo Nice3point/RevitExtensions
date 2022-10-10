7196fc8f2074f5be4aabd8acb9935cf78d4bb3eb
Nice3point <nice3point@gmail.com>
2022-10-10 12:55:16 +0300

    Release 2023.1.8

--- Build/Build.Properties.cs
+++ Build/Build.Properties.cs
@@ -2,11 +2,11 @@ partial class Build
 {
     readonly Dictionary<string, string> VersionMap = new()
     {
-        {"Release R19", "2019.1.7"},
-        {"Release R20", "2020.1.7"},
-        {"Release R21", "2021.1.7"},
-        {"Release R22", "2022.1.7"},
-        {"Release R23", "2023.1.7"},
+        {"Release R19", "2019.1.8"},
+        {"Release R20", "2020.1.8"},
+        {"Release R21", "2021.1.8"},
+        {"Release R22", "2022.1.8"},
+        {"Release R23", "2023.1.8"},
     };
 
     const string BuildConfiguration = "Release";
