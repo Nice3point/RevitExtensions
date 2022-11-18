9cb583158776954619bba066434fb3f22fbe8fcc
Nice3point <nice3point@gmail.com>
2022-11-19 02:51:33 +0300

    Update Readme.md

--- Build/Build.Properties.cs
+++ Build/Build.Properties.cs
@@ -2,11 +2,11 @@ partial class Build
 {
     readonly Dictionary<string, string> VersionMap = new()
     {
-        {"Release R19", "2019.1.9"},
-        {"Release R20", "2020.1.9"},
-        {"Release R21", "2021.1.9"},
-        {"Release R22", "2022.1.9"},
-        {"Release R23", "2023.1.9"},
+        {"Release R19", "2019.1.8"},
+        {"Release R20", "2020.1.8"},
+        {"Release R21", "2021.1.8"},
+        {"Release R22", "2022.1.8"},
+        {"Release R23", "2023.1.8"},
     };
 
     const string BuildConfiguration = "Release";
