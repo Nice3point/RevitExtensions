2635658e12ef3d479685384115a1eb5b1e1e2678
Nice3point <nice3point@gmail.com>
2024-03-22 14:27:55 +0300

    Context menu extensions

--- Nice3point.Revit.Extensions/RibbonExtensions.cs
+++ Nice3point.Revit.Extensions/RibbonExtensions.cs
@@ -2,8 +2,13 @@
 using System.Windows.Media.Imaging;
 using Autodesk.Revit.UI;
 using Autodesk.Windows;
+using ComboBox = Autodesk.Revit.UI.ComboBox;
 using RibbonButton = Autodesk.Revit.UI.RibbonButton;
 using RibbonPanel = Autodesk.Revit.UI.RibbonPanel;
+using TextBox = Autodesk.Revit.UI.TextBox;
+#if R25_OR_GREATER
+using ContextMenu = Autodesk.Revit.UI.ContextMenu;
+#endif
 
 // ReSharper disable InvertIf
 // ReSharper disable ForeachCanBeConvertedToQueryUsingAnotherGetEnumerator
@@ -82,8 +87,8 @@ public static class RibbonExtensions
     /// <exception cref="T:Autodesk.Revit.Exceptions.ArgumentException">Thrown when a PushButton already exists in the panel</exception>
     public static PushButton AddPushButton(this RibbonPanel panel, Type command, string buttonText)
     {
-        var pushButtonData = new PushButtonData(command.FullName, buttonText, Assembly.GetAssembly(command).Location, command.FullName);
-        return (PushButton) panel.AddItem(pushButtonData);
+        var pushButtonData = new PushButtonData(command.FullName, buttonText, Assembly.GetAssembly(command)!.Location, command.FullName);
+        return (PushButton)panel.AddItem(pushButtonData);
     }
 
     /// <summary>
@@ -94,8 +99,8 @@ public static class RibbonExtensions
     public static PushButton AddPushButton<TCommand>(this RibbonPanel panel, string buttonText) where TCommand : IExternalCommand, new()
     {
         var command = typeof(TCommand);
-        var pushButtonData = new PushButtonData(command.FullName, buttonText, Assembly.GetAssembly(command).Location, command.FullName);
-        return (PushButton) panel.AddItem(pushButtonData);
+        var pushButtonData = new PushButtonData(command.FullName, buttonText, Assembly.GetAssembly(command)!.Location, command.FullName);
+        return (PushButton)panel.AddItem(pushButtonData);
     }
 
     /// <summary>
@@ -106,7 +111,7 @@ public static class RibbonExtensions
     public static PulldownButton AddPullDownButton(this RibbonPanel panel, string internalName, string buttonText)
     {
         var pushButtonData = new PulldownButtonData(internalName, buttonText);
-        return (PulldownButton) panel.AddItem(pushButtonData);
+        return (PulldownButton)panel.AddItem(pushButtonData);
     }
 
     /// <summary>
@@ -117,7 +122,7 @@ public static class RibbonExtensions
     public static SplitButton AddSplitButton(this RibbonPanel panel, string internalName, string buttonText)
     {
         var pushButtonData = new SplitButtonData(internalName, buttonText);
-        return (SplitButton) panel.AddItem(pushButtonData);
+        return (SplitButton)panel.AddItem(pushButtonData);
     }
 
     /// <summary>
@@ -128,7 +133,7 @@ public static class RibbonExtensions
     public static RadioButtonGroup AddRadioButtonGroup(this RibbonPanel panel, string internalName)
     {
         var pushButtonData = new RadioButtonGroupData(internalName);
-        return (RadioButtonGroup) panel.AddItem(pushButtonData);
+        return (RadioButtonGroup)panel.AddItem(pushButtonData);
     }
 
     /// <summary>
@@ -139,7 +144,7 @@ public static class RibbonExtensions
     public static ComboBox AddComboBox(this RibbonPanel panel, string internalName)
     {
         var pushButtonData = new ComboBoxData(internalName);
-        return (ComboBox) panel.AddItem(pushButtonData);
+        return (ComboBox)panel.AddItem(pushButtonData);
     }
 
     /// <summary>
@@ -150,7 +155,7 @@ public static class RibbonExtensions
     public static TextBox AddTextBox(this RibbonPanel panel, string internalName)
     {
         var pushButtonData = new TextBoxData(internalName);
-        return (TextBox) panel.AddItem(pushButtonData);
+        return (TextBox)panel.AddItem(pushButtonData);
     }
 
     /// <summary>
@@ -160,7 +165,7 @@ public static class RibbonExtensions
     /// <exception cref="T:Autodesk.Revit.Exceptions.ArgumentException">Thrown when a PushButton already exists in the PushButton</exception>
     public static PushButton AddPushButton(this PulldownButton pullDownButton, Type command, string buttonText)
     {
-        var pushButtonData = new PushButtonData(command.FullName, buttonText, Assembly.GetAssembly(command).Location, command.FullName);
+        var pushButtonData = new PushButtonData(command.FullName, buttonText, Assembly.GetAssembly(command)!.Location, command.FullName);
         return pullDownButton.AddPushButton(pushButtonData);
     }
 
@@ -172,7 +177,7 @@ public static class RibbonExtensions
     public static PushButton AddPushButton<TCommand>(this PulldownButton pullDownButton, string buttonText) where TCommand : IExternalCommand, new()
     {
         var command = typeof(TCommand);
-        var pushButtonData = new PushButtonData(command.FullName, buttonText, Assembly.GetAssembly(command).Location, command.FullName);
+        var pushButtonData = new PushButtonData(command.FullName, buttonText, Assembly.GetAssembly(command)!.Location, command.FullName);
         return pullDownButton.AddPushButton(pushButtonData);
     }
 
@@ -211,4 +216,76 @@ public static class RibbonExtensions
         button.AvailabilityClassName = typeof(T).FullName;
         return button;
     }
+#if R25_OR_GREATER
+
+    /// <summary>
+    ///     Adds a menu to the Context Menu
+    /// </summary>
+    /// <param name="menu">Menu item owner</param>
+    /// <param name="name">The name will show on the menu</param>
+    /// <typeparam name="TCommand">Type inherited from <see cref="Autodesk.Revit.UI.IExternalCommand"/></typeparam>
+    public static ContextMenu AddMenuItem<TCommand>(this ContextMenu menu, string name) where TCommand : IExternalCommand, new()
+    {
+        var command = typeof(TCommand);
+        var menuItem = new CommandMenuItem(name, command.FullName, Assembly.GetAssembly(command)!.Location);
+        menu.AddItem(menuItem);
+
+        return menu;
+    }
+
+    /// <summary>
+    ///     Adds a separator to the Context Menu
+    /// </summary>
+    /// <param name="menu">Menu item owner</param>
+    public static ContextMenu AddSeparator(this ContextMenu menu)
+    {
+        var separator = new SeparatorItem();
+        menu.AddItem(separator);
+
+        return menu;
+    }
+
+    /// <summary>
+    ///     Adds a sub menu to the Context Menu
+    /// </summary>
+    /// <param name="menu">Menu item owner</param>
+    /// <param name="name">The name will show on menu</param>
+    /// <param name="subMenu">The child of Context Menu</param>
+    public static ContextMenu AddSubMenu(this ContextMenu menu, string name, ContextMenu subMenu)
+    {
+        var separator = new SubMenuItem(name, subMenu);
+        menu.AddItem(separator);
+
+        return menu;
+    }
+
+    /// <summary>
+    ///     Specifies the class that decides the availability of menu item
+    /// </summary>
+    /// <param name="menuItem">The menu item that will be restricted on the context menu</param>
+    /// <typeparam name="T">Type inherited from <see cref="Autodesk.Revit.UI.IExternalCommandAvailability"/></typeparam>
+    /// <remarks>Class T should share the same assembly with add-in External Command</remarks>
+    public static CommandMenuItem SetAvailabilityController<T>(this CommandMenuItem menuItem) where T : IExternalCommandAvailability, new()
+    {
+        menuItem.SetAvailabilityClassName(typeof(T).FullName);
+        return menuItem;
+    }
+
+    /// <summary>
+    ///     Register and configure the newly created Context Menu
+    /// </summary>
+    public static UIControlledApplication RegisterContextMenu(this UIControlledApplication application, Action<ContextMenu> handler)
+    {
+        var callingAssembly = Assembly.GetCallingAssembly();
+
+        var creator = new ContextMenuCreator(handler);
+        application.RegisterContextMenu(callingAssembly.FullName, creator);
+        return application;
+    }
+
+    private class ContextMenuCreator(Action<ContextMenu> handler) : IContextMenuCreator
+    {
+        public void BuildContextMenu(ContextMenu menu) => handler.Invoke(menu);
+    }
+#endif
 }
