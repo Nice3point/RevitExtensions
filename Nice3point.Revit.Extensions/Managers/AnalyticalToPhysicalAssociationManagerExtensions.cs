454d690525f3e5e0400e6d6c5ed676b551fbaa6b
Nice3point <nice3point@gmail.com>
2024-10-11 01:43:37 +0300

    AnalyticalToPhysicalAssociationManager Extensions

---
+++ Nice3point.Revit.Extensions/Managers/AnalyticalToPhysicalAssociationManagerExtensions.cs
@@ -0,0 +1,36 @@
+﻿#if REVIT2023_OR_GREATER
+using Autodesk.Revit.DB.Structure;
+
+// ReSharper disable once CheckNamespace
+namespace Nice3point.Revit.Extensions;
+
+/// <summary>
+///     Represent extension methods for the <see cref="Autodesk.Revit.DB.Structure.AnalyticalToPhysicalAssociationManager"/> class.
+/// </summary>
+public static class AnalyticalToPhysicalAssociationManagerExtensions
+{
+    /// <summary>Returns the AnalyticalToPhysicalAssociationManager for this document.</summary>
+    /// <param name="document">Revit document.</param>
+    [Pure]
+    public static AnalyticalToPhysicalAssociationManager GetAnalyticalToPhysicalAssociationManager(this Document document)
+    {
+        return AnalyticalToPhysicalAssociationManager.GetAnalyticalToPhysicalAssociationManager(document);
+    }
+
+    /// <summary>Returns true if the element is an analytical element.</summary>
+    /// <param name="element">The element to be checked.</param>
+    [Pure]
+    public static bool IsAnalyticalElement(this Element element)
+    {
+        return AnalyticalToPhysicalAssociationManager.IsAnalyticalElement(element.Document, element.Id);
+    }
+
+    /// <summary>Returns true if the element is a physical element.</summary>
+    /// <param name="element">The element to be checked.</param>
+    [Pure]
+    public static bool IsPhysicalElement(this Element element)
+    {
+        return AnalyticalToPhysicalAssociationManager.IsPhysicalElement(element.Document, element.Id);
+    }
+}
+#endif
