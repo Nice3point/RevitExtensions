4cd102c2f094d541ed542227d46041f3fa5f8b5e
Nice3point <nice3point@gmail.com>
2024-10-10 23:25:11 +0300

    BoundingBoxXyz extensions

---
+++ Nice3point.Revit.Extensions/BoundingBoxXyzExtensions.cs
@@ -0,0 +1,69 @@
+﻿namespace Nice3point.Revit.Extensions;
+
+/// <summary>
+///     Revit BoundingBoxXyz Extensions
+/// </summary>
+public static class BoundingBoxXyzExtensions
+{
+    /// <summary>
+    ///     Computes the geometric center point of the bounding box.
+    /// </summary>
+    /// <param name="boundingBox">The bounding box from which the centroid is calculated.</param>
+    /// <returns>A point representing the centroid of the bounding box.</returns>
+    public static XYZ ComputeCentroid(this BoundingBoxXYZ boundingBox)
+    {
+        return (boundingBox.Min + boundingBox.Max) / 2;
+    }
+
+    /// <summary>
+    ///     Retrieves the coordinates of the eight vertices that define the bounding box.
+    /// </summary>
+    /// <param name="boundingBox">The bounding box from which the vertices are computed.</param>
+    /// <returns>An array of points representing the vertices of the bounding box.</returns>
+    public static XYZ[] ComputeVertices(this BoundingBoxXYZ boundingBox)
+    {
+        return
+        [
+            new XYZ(boundingBox.Min.X, boundingBox.Min.Y, boundingBox.Min.Z),
+            new XYZ(boundingBox.Min.X, boundingBox.Min.Y, boundingBox.Max.Z),
+            new XYZ(boundingBox.Min.X, boundingBox.Max.Y, boundingBox.Min.Z),
+            new XYZ(boundingBox.Min.X, boundingBox.Max.Y, boundingBox.Max.Z),
+            new XYZ(boundingBox.Max.X, boundingBox.Min.Y, boundingBox.Min.Z),
+            new XYZ(boundingBox.Max.X, boundingBox.Min.Y, boundingBox.Max.Z),
+            new XYZ(boundingBox.Max.X, boundingBox.Max.Y, boundingBox.Min.Z),
+            new XYZ(boundingBox.Max.X, boundingBox.Max.Y, boundingBox.Max.Z)
+        ];
+    }
+
+    /// <summary>
+    ///     Calculates the volume enclosed by the bounding box.
+    /// </summary>
+    /// <param name="boundingBox">The bounding box for which the volume is calculated.</param>
+    /// <returns>The volume of the bounding box.</returns>
+    public static double ComputeVolume(this BoundingBoxXYZ boundingBox)
+    {
+        var length = boundingBox.Max.X - boundingBox.Min.X;
+        var width = boundingBox.Max.Y - boundingBox.Min.Y;
+        var height = boundingBox.Max.Z - boundingBox.Min.Z;
+
+        return length * width * height;
+    }
+
+    /// <summary>
+    ///     Calculates the total surface area of the bounding box.
+    /// </summary>
+    /// <param name="boundingBox">The bounding box for which the surface area is calculated.</param>
+    /// <returns>The total surface area of the bounding box.</returns>
+    public static double ComputeSurfaceArea(this BoundingBoxXYZ boundingBox)
+    {
+        var length = boundingBox.Max.X - boundingBox.Min.X;
+        var width = boundingBox.Max.Y - boundingBox.Min.Y;
+        var height = boundingBox.Max.Z - boundingBox.Min.Z;
+
+        var area1 = length * width;
+        var area2 = length * height;
+        var area3 = width * height;
+
+        return 2 * (area1 + area2 + area3);
+    }
+}
