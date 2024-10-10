e3aa173f6fe7ac169cf00c214b98904d66ab1b68
Nice3point <nice3point@gmail.com>
2024-10-11 00:45:38 +0300

    LightGroupManager Extensions

---
+++ Nice3point.Revit.Extensions/Managers/LightGroupManagerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB.Lighting;
+
+// ReSharper disable once CheckNamespace
+namespace Nice3point.Revit.Extensions;
+
+/// <summary>
+///     Revit LightGroupManager Extensions
+/// </summary>
+public static class LightGroupManagerExtensions
+{
+    /// <summary>Creates a light group manager object from the given document</summary>
+    /// <param name="document">The document the manager is from</param>
+    /// <returns>The newly created Light group manager object</returns>
+    /// <exception cref="T:Autodesk.Revit.Exceptions.ArgumentException">
+    ///    The document is not valid because it is not a project (rvt) document
+    /// </exception>
+    [Pure]
+    public static LightGroupManager GetLightGroupManager(this Document document)
+    {
+        return LightGroupManager.GetLightGroupManager(document);
+    }
+}
