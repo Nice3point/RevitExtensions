b82cff7208b9219a280c0a648311b13ecf09c783
Nice3point <nice3point@gmail.com>
2024-10-11 14:42:29 +0300

    SpatialFieldManager Extensions

--- Nice3point.Revit.Extensions/Managers/SpatialFieldManagerExtensions.cs
+++ Nice3point.Revit.Extensions/Managers/SpatialFieldManagerExtensions.cs
@@ -1,19 +1,37 @@
-﻿#if REVIT2022_OR_GREATER
+﻿using Autodesk.Revit.DB.Analysis;
+
 // ReSharper disable once CheckNamespace
 namespace Nice3point.Revit.Extensions;
 
 /// <summary>
-///     Represent extension methods for the <see cref="Autodesk.Revit.DB.TemporaryGraphicsManager"/> class.
+///     Represent extension methods for the <see cref="Autodesk.Revit.DB.Analysis.SpatialFieldManager"/> class.
 /// </summary>
-public static class TemporaryGraphicsManagerExtensions
+public static class SpatialFieldManagerExtensions
 {
-    /// <summary>Gets a TemporaryGraphicsManager reference of the document.</summary>
-    /// <param name="document">The document.</param>
-    /// <returns>Instance of TemporaryGraphicsManager.</returns>
-    [Pure]
-    public static TemporaryGraphicsManager GetTemporaryGraphicsManager(this Document document)
+    /// <summary>Factory method - creates manager object for the given view</summary>
+    /// <param name="view">View for which manager object is created or retrieved</param>
+    /// <param name="numberOfMeasurements">
+    ///    Total number of measurements in the calculated results.
+    ///    This number defines the length of value arrays in ValueAtPoint objects
+    /// </param>
+    /// <returns>Manager object for the view passed in the argument</returns>
+    /// <exception cref="T:Autodesk.Revit.Exceptions.ArgumentException">
+    ///    numberOfMeasurements is less than one
+    /// </exception>
+    /// <exception cref="T:Autodesk.Revit.Exceptions.InvalidOperationException">
+    ///    View is not allowed to display analysis results or a manager object for this view already exists
+    /// </exception>
+    public static SpatialFieldManager CreateSpatialFieldManager(this View view, int numberOfMeasurements)
     {
-        return TemporaryGraphicsManager.GetTemporaryGraphicsManager(document);
+        return SpatialFieldManager.CreateSpatialFieldManager(view, numberOfMeasurements);
+    }
+    
+    /// <summary>Retrieves manager object for the given view or returns NULL</summary>
+    /// <param name="view">View for which manager object is retrieved</param>
+    /// <returns>Manager object for the view passed in the argument</returns>
+    [Pure]
+    public static SpatialFieldManager? GetSpatialFieldManager(this View view)
+    {
+        return SpatialFieldManager.GetSpatialFieldManager(view);
     }
 }
-#endif
