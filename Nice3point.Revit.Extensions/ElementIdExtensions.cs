fa3ee065072c0ea88d2a327d86a6c5018cf1c67e
Nice3point <nice3point@gmail.com>
2024-10-13 02:02:48 +0300

    ToElements extensions

--- Nice3point.Revit.Extensions/ElementIdExtensions.cs
+++ Nice3point.Revit.Extensions/ElementIdExtensions.cs
@@ -3,19 +3,15 @@
 /// <summary>
 ///     Revit Element Extensions
 /// </summary>
+[PublicAPI]
 public static class ElementIdExtensions
 {
     /// <summary>
-    ///     Gets the Element referenced by the input ElementId
+    ///     Retrieves the Element associated with the specified ElementId.
     /// </summary>
-    /// <param name="document">Document associated with Element</param>
-    /// <param name="id">
-    ///     The ElementId, whose referenced Element will be retrieved from the model.
-    /// </param>
-    /// <returns>The element referenced by the input argument</returns>
-    /// <remarks>
-    ///     <see langword="null" /> will be returned if the input ElementId doesn't reference to a valid Element.
-    /// </remarks>
+    /// <param name="document">The document containing the element.</param>
+    /// <param name="id">The ElementId of the element to retrieve.</param>
+    /// <returns>The element associated with the specified ElementId, or <see langword="null"/> if the ElementId is invalid.</returns>
     [Pure]
     public static Element? ToElement(this ElementId id, Document document)
     {
@@ -23,21 +19,88 @@ public static class ElementIdExtensions
     }
 
     /// <summary>
-    ///     Gets the Element referenced by the input ElementId and cast to type T
+    ///     Retrieves the Element associated with the specified ElementId as the specified type T.
     /// </summary>
-    /// <param name="document">Document associated with Element</param>
-    /// <param name="id">
-    ///     The ElementId, whose referenced Element will be retrieved from the model
-    /// </param>
-    /// <typeparam name="T">A type derived from Element</typeparam>
-    /// <returns>The element referenced by the input argument, casted to type T</returns>
-    /// <remarks>
-    ///     <see langword="null" /> will be returned if the input ElementId doesn't reference to a valid Element.
-    /// </remarks>
+    /// <typeparam name="T">The expected type of the element.</typeparam>
+    /// <param name="document">The document containing the element.</param>
+    /// <param name="id">The ElementId of the element to retrieve.</param>
+    /// <returns>The element of type T associated with the specified ElementId, or <see langword="null"/> if the ElementId is invalid.</returns>
     [Pure]
-    public static T ToElement<T>(this ElementId id, Document document) where T : Element
+    public static T? ToElement<T>(this ElementId id, Document document) where T : Element
     {
-        return (T) document.GetElement(id);
+        return (T?)document.GetElement(id);
+    }
+
+    /// <summary>
+    ///     Retrieves a collection of Elements associated with the specified ElementIds.
+    /// </summary>
+    /// <param name="document">The document containing the elements.</param>
+    /// <param name="elementIds">The collection of ElementIds to retrieve the elements for.</param>
+    /// <returns>A list of elements associated with the specified ElementIds.</returns>
+    [Pure]
+    public static IList<Element> ToElements(this ICollection<ElementId> elementIds, Document document)
+    {
+        var elementTypes = new FilteredElementCollector(document, elementIds).WhereElementIsElementType();
+        var elementInstances = new FilteredElementCollector(document, elementIds).WhereElementIsNotElementType();
+        return elementTypes.UnionWith(elementInstances).ToElements();
+    }
+
+    /// <summary>
+    ///     Retrieves a collection of Elements associated with the specified ElementIds as the specified type T.
+    /// </summary>
+    /// <typeparam name="T">The expected type of the elements.</typeparam>
+    /// <param name="document">The document containing the elements.</param>
+    /// <param name="elementIds">The collection of ElementIds to retrieve the elements for.</param>
+    /// <returns>A list of elements of type T associated with the specified ElementIds.</returns>
+    [Pure]
+    public static IList<T> ToElements<T>(this ICollection<ElementId> elementIds, Document document) where T : Element
+    {
+        var elementTypes = new FilteredElementCollector(document, elementIds).WhereElementIsElementType();
+        var elementInstances = new FilteredElementCollector(document, elementIds).WhereElementIsNotElementType();
+        return Enumerable.Cast<T>(elementTypes.UnionWith(elementInstances)).ToList();
+    }
+    
+    /// <summary>
+    ///     Retrieves the Elements associated with the specified ElementIds in their original order.
+    /// </summary>
+    /// <param name="document">The document containing the elements.</param>
+    /// <param name="elementIds">The collection of ElementIds to retrieve the elements for.</param>
+    /// <returns>A list of elements in the same order as the input ElementIds.</returns>
+    [Pure]
+    public static IList<Element> ToOrderedElements(this ICollection<ElementId> elementIds, Document document)
+    {
+        var elements = elementIds.ToElements(document);
+        var elementDictionary = elements.ToDictionary(element => element.Id);
+
+        var orderedElements = new List<Element>(elementIds.Count);
+        foreach (var id in elementIds)
+        {
+            orderedElements.Add(elementDictionary[id]);
+        }
+
+        return orderedElements;
+    }
+    
+    /// <summary>
+    ///     Retrieves the Elements associated with the specified ElementIds and casts them to the specified type T in their original order.
+    /// </summary>
+    /// <typeparam name="T">The target type derived from Element.</typeparam>
+    /// <param name="document">The document containing the elements.</param>
+    /// <param name="elementIds">The collection of ElementIds to retrieve the elements for.</param>
+    /// <returns>A list of elements of type T in the same order as the input ElementIds.</returns>
+    [Pure]
+    public static IList<T> ToOrderedElements<T>(this ICollection<ElementId> elementIds, Document document) where T : Element
+    {
+        var elements = elementIds.ToElements<T>(document);
+        var elementDictionary = elements.ToDictionary(element => element.Id);
+
+        var orderedElements = new List<T>(elementIds.Count);
+        foreach (var id in elementIds)
+        {
+            orderedElements.Add(elementDictionary[id]);
+        }
+
+        return orderedElements;
     }
 
     /// <summary>
@@ -47,7 +110,7 @@ public static class ElementIdExtensions
     public static bool AreEquals(this ElementId elementId, BuiltInCategory category)
     {
 #if REVIT2024_OR_GREATER
-        return elementId.Value == (long) category;
+        return elementId.Value == (long)category;
 #else
         return elementId.IntegerValue == (int) category;
 #endif
@@ -60,7 +123,7 @@ public static class ElementIdExtensions
     public static bool AreEquals(this ElementId elementId, BuiltInParameter parameter)
     {
 #if REVIT2024_OR_GREATER
-        return elementId.Value == (long) parameter;
+        return elementId.Value == (long)parameter;
 #else
         return elementId.IntegerValue == (int) parameter;
 #endif
