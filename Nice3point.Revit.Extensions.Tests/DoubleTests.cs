7d24669edf7d2b75dd39a9c0f486e080134fc4be
Nice3point <nice3point@gmail.com>
2024-10-13 16:08:08 +0300

    Setup xUnit

--- Nice3point.Revit.Extensions.Tests/DoubleTests.cs
+++ Nice3point.Revit.Extensions.Tests/DoubleTests.cs
@@ -1,22 +1,19 @@
-﻿using NUnit.Framework;
+﻿namespace Nice3point.Revit.Extensions.Tests;
 
-namespace Nice3point.Revit.Extensions.Tests;
-
-[TestFixture]
-public class DoubleTests
+public sealed class DoubleTests
 {
-    [TestCase]
+    [Fact]
     public void IsAlmostEqualTest()
     {
-        Assert.AreEqual(true, 0.09999.IsAlmostEqual(0.1, 1e-3));
-        Assert.AreEqual(true, 0.063636.IsAlmostEqual(0.06, 1e-2));
-        Assert.AreEqual(true, 1e-15.IsAlmostEqual(0));
+        Assert.True(0.09999d.IsAlmostEqual(0.1, 1e-3));
+        Assert.True(0.063636.IsAlmostEqual(0.06, 1e-2));
+        Assert.True(1e-15.IsAlmostEqual(0));
     }
 
-    [TestCase]
+    [Fact]
     public void RoundTest()
     {
-        Assert.AreEqual(0, 1e-15.Round());
-        Assert.AreEqual(0.1, 0.099999.Round(2));
+        Assert.Equal(0, 1e-15.Round(), 1);
+        Assert.Equal(0.1, 0.099999.Round(2), 1);
     }
 }
