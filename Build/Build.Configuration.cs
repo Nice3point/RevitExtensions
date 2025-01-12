ebf629c5ce97f5d88a9fde85c017976d4161cec9
Nice3point <nice3point@gmail.com>
2025-01-12 19:04:03 +0300

    Bump version

--- Build/Build.Configuration.cs
+++ Build/Build.Configuration.cs
@@ -16,12 +16,12 @@ sealed partial class Build
 
         PackageVersionsMap = new()
         {
-            { "Release R20", "2020.3.1-preview.5.1" },
-            { "Release R21", "2021.3.1-preview.5.1" },
-            { "Release R22", "2022.3.1-preview.5.1" },
-            { "Release R23", "2023.3.1-preview.5.1" },
-            { "Release R24", "2024.1.1-preview.5.1" },
-            { "Release R25", "2025.0.1-preview.5.1" },
+            { "Release R20", "2020.3.1-preview.5.2" },
+            { "Release R21", "2021.3.1-preview.5.2" },
+            { "Release R22", "2022.3.1-preview.5.2" },
+            { "Release R23", "2023.3.1-preview.5.2" },
+            { "Release R24", "2024.1.1-preview.5.2" },
+            { "Release R25", "2025.0.1-preview.5.2" },
         };
     }
 }
