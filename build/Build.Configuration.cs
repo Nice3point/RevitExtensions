482fd4f80154a474ebecd917f9d4a8b76f913a58
Nice3point <nice3point@gmail.com>
2025-02-19 18:16:44 +0300

    Bump version

--- build/Build.Configuration.cs
+++ build/Build.Configuration.cs
@@ -16,13 +16,13 @@
 
         PackageVersionsMap = new()
         {
-            { "Release R20", "2020.4.0-preview.1.250122" },
-            { "Release R21", "2021.4.0-preview.1.250122" },
-            { "Release R22", "2022.4.0-preview.1.250122" },
-            { "Release R23", "2023.4.0-preview.1.250122" },
-            { "Release R24", "2024.2.0-preview.1.250122" },
-            { "Release R25", "2025.1.0-preview.1.250122" },
-            { "Release R26", "2026.0.0-preview.1.250122" },
+            { "Release R20", "2020.4.0-preview.2.250219" },
+            { "Release R21", "2021.4.0-preview.2.250219" },
+            { "Release R22", "2022.4.0-preview.2.250219" },
+            { "Release R23", "2023.4.0-preview.2.250219" },
+            { "Release R24", "2024.2.0-preview.2.250219" },
+            { "Release R25", "2025.1.0-preview.2.250219" },
+            { "Release R26", "2026.0.0-preview.2.250219" },
         };
     }
 }
